b6faeaabcea3e12e1807e0325667265ffab8a639
Oyewole Abayomi Samuel <oyewoleabayomi@gmail.com>
2019-11-10 02:22:26 +0100

    fix lint

--- controllers/contact.controller.js
+++ controllers/contact.controller.js
@@ -4,72 +4,72 @@ const JoiHelper	= require('../helper/joi.helper')
 const Contact 	= require('../models/contact.model')
 
 module.exports = {
-  async createContact(req, res){
-    const {id} = req.user
+	async createContact(req, res){
+		const {id} = req.user
 
-    try{
-      const data = _.pick(req.body, [
-        'first_name',
-        'last_name',
-        'phone_number',
-        'street',
-        'city',
-        'state',
-        'country',
-        'postal_code'
-      ])
+		try{
+			const data = _.pick(req.body, [
+				'first_name',
+				'last_name',
+				'phone_number',
+				'street',
+				'city',
+				'state',
+				'country',
+				'postal_code'
+			])
 
-      const {error, value} = Joi.validate(data,  JoiHelper.validateContact())
-      if(error) throw new Error(error.message)
+			const {error, value} = Joi.validate(data,  JoiHelper.validateContact())
+			if(error) throw new Error(error.message)
 
-      let contact = new Contact(value)
-      contact.user = id
-      let savedContact = await contact.save()
+			let contact = new Contact(value)
+			contact.user = id
+			let savedContact = await contact.save()
 
-      res.status(200).json(savedContact)
+			res.status(200).json(savedContact)
 
-    }catch(error){
-      return res.status(400).json({msg: error.message})
-    }
-  },
+		}catch(error){
+			return res.status(400).json({msg: error.message})
+		}
+	},
 
-  async updateContact(req, res){
+	async updateContact(req, res){
 
-    try {
-      const {id} = req.params
-      const data = _.pick(req.body, [
-        'first_name',
-        'last_name',
-        'phone_number',
-        'street',
-        'city',
-        'state',
-        'country',
-        'postal_code'
-      ])
+		try {
+			const {id} = req.params
+			const data = _.pick(req.body, [
+				'first_name',
+				'last_name',
+				'phone_number',
+				'street',
+				'city',
+				'state',
+				'country',
+				'postal_code'
+			])
 
-      const {error, value} = Joi.validate(data,  JoiHelper.validateContact())
-      if(error) throw new Error(error.message)
+			const {error, value} = Joi.validate(data,  JoiHelper.validateContact())
+			if(error) throw new Error(error.message)
 
-      let contact = await Contact.findOneAndUpdate({_id: id}, {$set: value}, {new: true})
-      if(!contact) throw new Error(`Contact with ID: ${id} not found!`)
+			let contact = await Contact.findOneAndUpdate({_id: id}, {$set: value}, {new: true})
+			if(!contact) throw new Error(`Contact with ID: ${id} not found!`)
 
-      return res.status(200).json(contact)
+			return res.status(200).json(contact)
 
-    } catch (error) {
-      return res.status(400).json({msg: error.message})
-    }
-  },
+		} catch (error) {
+			return res.status(400).json({msg: error.message})
+		}
+	},
 
-  async getAllContacts(req, res){
-    const {page, pageSize} = req.query
+	async getAllContacts(req, res){
+		const {page, pageSize} = req.query
 
-    let ipage       = parseInt( page || 1 )
-    let limit       = parseInt( pageSize || 25 )
-    let offset      = parseInt((ipage - 1), 10) * limit
+		let ipage       = parseInt( page || 1 )
+		let limit       = parseInt( pageSize || 25 )
+		let offset      = parseInt((ipage - 1), 10) * limit
 
-    let contacts = await Contact.paginate({}, { offset, limit })
-    res.status(200).json(contacts)
-  }
+		let contacts = await Contact.paginate({}, { offset, limit })
+		res.status(200).json(contacts)
+	}
 
 }
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -4,42 +4,42 @@ const JoiHelper	= require('../helper/joi.helper')
 const User 			= require('../models/user.model')
 
 module.exports = {
-  async register(req, res){
-    try {
-      const payload = _.pick(req.body, ['email', 'password'])
+	async register(req, res){
+		try {
+			const payload = _.pick(req.body, ['email', 'password'])
 
-      const {error, value} = Joi.validate(payload,  JoiHelper.validateLogin())
-      if(error) throw new Error(error.message)
+			const {error, value} = Joi.validate(payload,  JoiHelper.validateLogin())
+			if(error) throw new Error(error.message)
 
-      const user = await User.findOne({email: value.email})
-      if(user === null) throw new Error('Invalid credentials')
+			const user = await User.findOne({email: value.email})
+			if(user === null) throw new Error('Invalid credentials')
 
-      if(!user.validatePassword(value.password)){
-        throw new Error('Invalid credentials')
-      }
+			if(!user.validatePassword(value.password)){
+				throw new Error('Invalid credentials')
+			}
 
-      const data = user.toAuthJSON()
-      return res.status(200).json(data)
+			const data = user.toAuthJSON()
+			return res.status(200).json(data)
 
-    } catch (error) {
-      return res.status(400).json({msg: error.message})
-    }
-  },
+		} catch (error) {
+			return res.status(400).json({msg: error.message})
+		}
+	},
 
-  async login(req, res){
-    const data = _.pick(req.body, ['username', 'email', 'password', 'password_confirmation'])
+	async login(req, res){
+		const data = _.pick(req.body, ['username', 'email', 'password', 'password_confirmation'])
 
-    try {
-      const {error, value} = Joi.validate(data, JoiHelper.validateReg())
-      if(error) throw new Error(error.message)
+		try {
+			const {error, value} = Joi.validate(data, JoiHelper.validateReg())
+			if(error) throw new Error(error.message)
 
-      let user = new User({...value})
-      let saveUser = await user.save()
+			let user = new User({...value})
+			let saveUser = await user.save()
 
-      res.status(200).json(_.pick(saveUser, ['_id', 'username', 'email', 'createdAt', 'updatedAt']))
-    } catch (error) {
-      return res.status(500).json({msg: error.message})
-    }
-  }
+			res.status(200).json(_.pick(saveUser, ['_id', 'username', 'email', 'createdAt', 'updatedAt']))
+		} catch (error) {
+			return res.status(500).json({msg: error.message})
+		}
+	}
 
 }
